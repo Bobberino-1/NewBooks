42990e28d2ec58a1fdfe14ebb2fb17e07112d893
Robert Luse <Robert.Luse.Contractor@pepsico.com>
2022-01-14 09:59:13 -0600

    Fix to get all current queries working.

--- api/queries.py
+++ api/queries.py
@@ -12,61 +12,9 @@ def author_with_id(_, info, author_id):
 # type defined in the schema, we need to implement a resolver
 # for that field as well.
 def resolve_books_for_author(author, info):
-    books = db.session.query(Book.id, Book.name).join(Author, Book.author_id == Author.id).all()
+    books = db.session.query(Book.id, Book.name).join(Author, Book.author_id == Author.id). \
+        filter(Book.author_id == author.id).all()
     return books
-
-
-'''
-@convert_kwargs_to_snake_case
-def resolve_books(obj, info):
-    try:
-        books = [book.to_dict() for book in Book.query.all()]
-        payload = {
-            "success": True,
-            "books": books
-        }
-    except Exception as error:
-        payload = {
-            "success": False,
-            "errors": [str(error)]
-        }
-    return payload
-
-
-# This will now display the author as well
-@convert_kwargs_to_snake_case
-def resolve_book(obj, info, book_id):
-    try:
-        # book = Book.query.get(book_id)
-        # book = Book.query.join(Author, Author.id == Book.author_id).filter(Author.id==book_id).first()
-        # book = db.session.query(Book.name, Author.last_name).join(Author, Book.author_id == Author.id).all()
-        book = db.session.query(Book.id, Book.name, Author.id, Author.first_name, Author.last_name).join(Author, Book.author_id == Author.id).\
-            filter(Book.id == book_id).first()
-        payload = {
-            "success": True,
-            "book": book,
-        }
-
-    except Exception as error:  # book not found
-        payload = {
-            "success": False,
-            "errors": [f"{error}"]
-            # "errors": [f"Book item matching id {book_id} not found"]
-        }
-    print("in querie.resolve_book payload is: ", payload)
-    print("in querie.resolve_book payload book is: ", payload.items())
-    for key, val in payload.items():
-        print(key, val)
-        if key == 'book':
-            print(val[0])
-            print(val[1])
-            print(val[2])
-            print(val[3])
-            print(val[4])
-        if key == 'author':
-            print(val)
-
-    return payload
 
 
 @convert_kwargs_to_snake_case
@@ -84,22 +32,50 @@ def resolve_authors(obj, info):
         }
     return payload
 
+@convert_kwargs_to_snake_case
+def resolve_books(obj, info):
+    try:
+        books = [book.to_dict() for book in Book.query.all()]
+        payload = {
+            "success": True,
+            "books": books
+        }
+    except Exception as error:
+        payload = {
+            "success": False,
+            "errors": [str(error)]
+        }
+    return payload
+
+
+@convert_kwargs_to_snake_case
+def resolve_book(obj, info, book_id):
+    try:
+        book = Book.query.get(book_id)
+        payload = {
+            "success": True,
+            "book": book,
+        }
+    except Exception as error:  # book not found
+        payload = {
+            "success": False,
+            "errors": [f"{error}"]
+            # "errors": [f"Book item matching id {book_id} not found"]
+        }
+
+    return payload
 
 @convert_kwargs_to_snake_case
 def resolve_author(obj, info, author_id):
-    print("resolve author is called")
     try:
         author = Author.query.get(author_id)
         payload = {
             "success": True,
             "author": author.to_dict()
         }
-
     except AttributeError:  # todo not found
         payload = {
             "success": False,
             "errors": [f"Author item matching id {author_id} not found"]
         }
-
     return payload 
-'''
--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from ariadne import load_schema_from_path, make_executable_schema, \
     graphql_sync, snake_case_fallback_resolvers, ObjectType, QueryType
 from ariadne.constants import PLAYGROUND_HTML
 from flask import request, jsonify
-from api.queries import author_with_id, resolve_books_for_author  # resolve_books, resolve_book, resolve_authors, resolve_author
+from api.queries import author_with_id, resolve_books_for_author, resolve_authors, \
+    resolve_books, resolve_book, resolve_author
 from api.mutations import resolve_create_book, resolve_delete_book, \
-                            resolve_create_author, resolve_delete_author  # , resolve_update_due_date
+    resolve_create_author, resolve_delete_author
 
 
 # binds
@@ -17,17 +18,17 @@ book=ObjectType("Book")
 query.set_field("author_with_id", author_with_id)
 author.set_field("books", resolve_books_for_author)
 
-'''
-query.set_field("books", resolve_books)
-query.set_field("book", resolve_book)
 query.set_field("authors", resolve_authors)
 query.set_field("author", resolve_author)
-'''
+query.set_field("books", resolve_books)
+query.set_field("book", resolve_book)
+
+
+
 
 mutation = ObjectType("Mutation")
 mutation.set_field("createBook", resolve_create_book)
 mutation.set_field("deleteBook", resolve_delete_book)
-# mutation.set_field("updateDueDate", resolve_update_due_date)
 mutation.set_field("createAuthor", resolve_create_author)
 mutation.set_field("deleteAuthor", resolve_delete_author)
 
